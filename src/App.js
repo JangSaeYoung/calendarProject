e89afa4c4f1bed7af448579b5ea6079f0e1817ad
saeyoung <syoungbb25@gmail.com>
2022-02-15 23:07:11 +0900

    🗓 Calendar Branch 설정 및 DayPicker 설치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import Calendar from'./Calendar';
+import styled from 'styled-components';
+
 
 function App() {
   return (
-  <div>화면 확인</div>
+  <Container>
+    <Sidebar>사이드바 자리</Sidebar>
+    <CalContainer>
+      <Calendar />
+    </CalContainer>
+  </Container>
   );
 }
 
 export default App;
+
+// 전체 기본 화면
+const Container = styled.div` 
+display: flex;
+`;
+
+const Sidebar = styled.div`
+width: 377px;
+height: 977px;
+background-color: #E2E2E2;
+`;
+
+const CalContainer= styled.div`
+// padding: 157px 82px;
+`
---
+++ src/Calendar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+// import styled from 'styled-components';
+import './Calendar.css';
+
+
+//calendar 레이아웃 컴포넌트
+// Daypicker를 import
+
+const Calendar = () => {
+    return(
+        <DayPicker />
+)
+};
+
+export default Calendar;
+
