b904567bf3a7636a44ca0655466253e3b5b3b7a4
saeyoung <syoungbb25@gmail.com>
2022-02-24 14:19:30 +0900

    calendar 위치조정

--- src/App.js
+++ src/App.js
@@ -16,10 +16,13 @@ export default App;
 
 // 전체 기본 화면
 const Container = styled.div` 
-display: flex;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const Sidebar = styled.div`
+display: flex;
 width: 23.5625rem;
 height: 61.0625rem;
 background-color: #E2E2E2;
